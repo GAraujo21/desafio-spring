45523814ffae5238308186d37130d76a0b2a33d6
GAraujo21 <gabrielimaaraujo@gmail.com>
2023-08-30 14:03:36 -0300

    criação da pasta DTOResponse e DTORequest

---
+++ desafio-springboot-next2023/src/main/java/com/example/desafiospringbootnext2023/DTO/DTORequest/DTOClienteRequest.java
@@ -0,0 +1,5 @@
+package com.example.desafiospringbootnext2023.DTO.DTORequest;
+
+public class DTOClienteRequest {
+    
+}
---
+++ desafio-springboot-next2023/src/main/java/com/example/desafiospringbootnext2023/DTO/DTOResponse/DTOClienteResponse.java
@@ -0,0 +1,5 @@
+package com.example.desafiospringbootnext2023.DTO.DTOResponse;
+
+public class DTOClienteResponse {
+    
+}
