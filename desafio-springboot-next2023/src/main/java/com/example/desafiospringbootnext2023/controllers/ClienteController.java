608eb1a79923d3d2b3a9aea0707936ef84e1a039
GAraujo21 <gabrielimaaraujo@gmail.com>
2023-08-30 11:41:28 -0300

    arrumando o Put e Delete byId e excluindo comentarios

--- desafio-springboot-next2023/src/main/java/com/example/desafiospringbootnext2023/controllers/ClienteController.java
+++ desafio-springboot-next2023/src/main/java/com/example/desafiospringbootnext2023/controllers/ClienteController.java
@@ -27,102 +27,54 @@ public class ClienteController {
     public ClienteRepository clienteRepository;
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<Cliente> listAll(){
-        return clienteRepository.findAll();
+    public ResponseEntity<List<Cliente>> getProdutos() {
+        return new ResponseEntity<List<Cliente>>(clienteRepository.findAll(), HttpStatus.OK);   
     }
-    /* Este modelo da maior controle na resposta HTTP do que o de cima...
-    @GetMapping
-    public ResponseEntity<List<Produto>> getProdutos() {
-        return new ResponseEntity<List<Produto>>(produtoRepository.findAll(), HttpStatus.OK);   
-    } */
     
-
     @GetMapping("/{id}")
     public ResponseEntity<Cliente> getCliente(@PathVariable Long id){
-        Optional<Cliente> resp = clienteRepository.findById(id); // repete
+        Optional<Cliente> resp = clienteRepository.findById(id);
         if(resp.isPresent()){
             return new ResponseEntity<Cliente>(resp.get(), HttpStatus.OK);
         }else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     } 
-    /*Calabria
-    @GetMapping ("/{id}")
-    public Cliente buscar(@PathVariable Long id){
-        Optional<cliente> resp = repository.findById(id);
-        if (resp.isEmpty()) {
-            return null;
-        } else {
-            return resp.get();
-        }
-    } */
 
-    /* GERSON
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Cliente findById(@PathVariable Long id) throws Exception{
-        return clienteRepository.findById(id).orElseThrow();
-    } */
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente){
-        return clienteRepository.save(cliente);
-    }
-
-    /* OUTRA MANEIRA mais especifica na resposta HTTP.
      @PostMapping
-    public ResponseEntity<Produto> addProduto(@RequestBody Produto produto) {
-        produtoRepository.save(produto);
-        return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
-    } */
+    public ResponseEntity<Cliente> addProduto(@RequestBody Cliente cliente) {
+        clienteRepository.save(cliente);
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+    }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente cliente){
-        Optional<Cliente> respOptional = clienteRepository.findById(id); // repete
-        if (respOptional.isEmpty()){
-            return ResponseEntity.notFound().build(); // build funciona?
-        }else{
+        Optional<Cliente> respOptional = clienteRepository.findById(id);
+        if (respOptional.isPresent()){
             Cliente clienteExistente = respOptional.get();
             clienteExistente.setName(cliente.getName());
             clienteExistente.setAge(cliente.getAge());
             clienteExistente.setMarital_status(cliente.getMarital_status());
             clienteExistente.setDependents(cliente.getDependents());
             clienteExistente.setIncome(cliente.getIncome());
-            //clienteExistente.setHouses(cliente.getHouses());
-            //clienteExistente.setVehicles(cliente.getVehicles());
             
-            Cliente clienteAtualizado = clienteRepository.save(clienteExistente);
-            return ResponseEntity.ok(clienteAtualizado);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Cliente> delete(@PathVariable Long id){
-        Optional<Cliente> respOptional = clienteRepository.findById(id);  // repete
-        if(respOptional.isEmpty()){
-            return ResponseEntity.notFound().build(); //404 Not Found
+            clienteRepository.save(clienteExistente);
+            return new ResponseEntity<>(clienteExistente, HttpStatus.OK);
         }else{
-            Cliente clienteDeletar = respOptional.get();
-            clienteRepository.delete(clienteDeletar);
-            return ResponseEntity.ok().build();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    /* POSSIVELMENTE uma forma melhor.
     @DeleteMapping("/{id}")
     public ResponseEntity<Cliente> deleteClinete(@PathVariable Long id) {
         Optional<Cliente> respOptional = clienteRepository.findById(id);
-
-        if (clienteRepository.isPresent()) {
-            ClienteRepository.deleteById(id);
+        if (respOptional.isPresent()) {
+            clienteRepository.deleteById(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-    */
 
 }
